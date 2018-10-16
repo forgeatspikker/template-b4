2c2be379fe6a2dc454150f9ddf480fed305725e9
Douwe Pausma <dpausma@gbu.nl>
2018-10-16 12:22:09 +0200

    Added: Scroll to top button.

--- index.php
+++ index.php
@@ -48,6 +48,8 @@ $lang = explode("-", $lang->getTag())[0];
     </nav>
 
     <h1 data-aos="fade-up">Hello, world!</h1>
+    
+    <div class="scroll-to-top" data-spy="affix" data-offset-top="200"><a href="#page" class="smooth-scroll-top"><i class="fa fa-angle-up"></i></a></div>
     <script type="text/javascript" src="<?= $path ?>/dist/js/bundle.js"></script>
   </body>
 </html>
