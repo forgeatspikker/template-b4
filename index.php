caee10bcfbf3d6501856b28fe9ebc32556e6d6d8
Douwe Pausma <dpausma@gbu.nl>
2018-12-05 12:00:57 +0100

    added fontawesome to toastr

--- index.php
+++ index.php
@@ -38,18 +38,27 @@ $lang = explode("-", $lang->getTag())[0];
   </head>
   <body id="page">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Logo</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <i class="fas fa-bars"></i>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <jdoc:include type="modules" name="main-menu" style="xhtml" />
+      <div class="container">
+        <a class="navbar-brand" href="#">Logo</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <jdoc:include type="modules" name="main-menu" style="xhtml" />
+        </div>
       </div>
     </nav>
+    <div class="container">
+      <jdoc:include type="message" />
+      <jdoc:include type="component" />
 
-    <h1 data-aos="fade-up">Hello, world!</h1>
-    <button class="btn btn-primary" onclick="toastr.info('Toastr werkt!!!');">Toastr Test</button>
+      <h1 data-aos="fade-up">AOS works (refresh)</h1>
 
+      <button class="btn btn-danger" onclick="toastr.error('Toastr werkt!');">Toastr Error Test</button>
+      <button class="btn btn-success" onclick="toastr.success('Toastr werkt!');">Toastr Succes Test</button>
+      <button class="btn btn-info" onclick="toastr.info('Toastr werkt!');">Toastr Info Test</button>
+      <button class="btn btn-warning" onclick="toastr.warning('Toastr werkt!');">Toastr Warning Test</button>
+    </div>
     <div class="scroll-to-top" data-spy="affix" data-offset-top="200"><a href="#page" class="smooth-scroll"><i class="fa fa-angle-up"></i></a></div>
     <script type="text/javascript" src="<?= $path ?>/dist/js/bundle.js"></script>
   </body>
